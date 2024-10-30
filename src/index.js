f5f10cfba4395675054efcdf0f100ab371fd7833
jonmei-G <136102255+jonmei-G@users.noreply.github.com>
2024-10-30 12:45:34 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["docker.jojo.link]: dockerHub,
+  ["quay.jojo.link"]: "https://quay.io",
+  ["gcr.jojo.link" ]: "https://gcr.io",
+  ["k8s-gcr.jojo.link"]: "https://k8s.gcr.io",
+  ["k8s.jojo.link"]: "https://registry.k8s.io",
+  ["ghcr.jojo.link" ]: "https://ghcr.io",
+  ["cloudsmith.jojo.link"]: "https://docker.cloudsmith.io",
+  ["ecr.jojo.link"]: "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  ["docker-staging.jojo.link" ]: dockerHub,
 };
 
 function routeByHosts(host) {
